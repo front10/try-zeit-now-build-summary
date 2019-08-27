d0b85d319144283ae7db81a3616018feacd3bcd5
Ivan <belzee92@gmail.com>
2019-08-27 16:55:46 -0400

    fix clean unnecesary props

--- components/DestinationSpace/DestinationSpace.js
+++ components/DestinationSpace/DestinationSpace.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import useStyles from "@front10/helpers/dist/UseStyles";
 import ImageHeader from "../ImageHeader";
-import Header from "../Header";
 import Box from "../Box";
 import Text from "../Text";
 import DestinationItem from "../DestinationItem";
@@ -13,24 +12,12 @@ const DestinationSpace = props => {
     image,
     name,
     data,
-    componentSettings: { itemSettings },
-    header,
-    showImage,
-    onKeyDown,
-    showCityName,
-    focusReferences,
-    focusable
+    itemSettings,
+    showImage
   } = props;
   const [className, style] = useStyles(props);
   return (
-    <Box
-      focusable={focusable}
-      tabIndex={focusable ? 0 : -1}
-      className={`${className} fr-destination-item`}
-      style={style}
-      focusReferences={focusReferences}
-      onKeyDown={onKeyDown}
-    >
+    <Box className={`${className} fr-destination-item`} style={style}>
       {showImage && (
         <ImageHeader {...imageProps} image={image}>
           <Text {...labelStyle}>{name}</Text>
@@ -61,37 +48,9 @@ DestinationSpace.propTypes = {
    */
   data: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)),
   /**
-   * Component that is going to render
-   */
-  componentSettings: PropTypes.objectOf(PropTypes.any),
-  /**
-   * Header props
-   */
-  header: PropTypes.objectOf(PropTypes.any),
-  /**
    * Determine if the image will be displayed in the component.
    */
-  showImage: PropTypes.bool,
-  /**
-   * Show or hide the city name
-   */
-  showCityName: PropTypes.bool,
-  /**
-   * OnKeyDown function.
-   */
-  onKeyDown: PropTypes.func,
-  /**
-   * FocusReferences functions
-   */
-  focusReferences: PropTypes.func,
-  /**
-   * Turn off or on tabIndex in the DestinationSpace
-   */
-  focusable: PropTypes.bool,
-  /**
-   * Show header in two lines
-   */
-  showHeaderBreak: PropTypes.bool
+  showImage: PropTypes.bool
 };
 DestinationSpace.defaultProps = {
   image: "",
@@ -100,14 +59,7 @@ DestinationSpace.defaultProps = {
   imageProps: {
     rounded: "2"
   },
-  componentSettings: {},
-  header: {},
-  showImage: true,
-  showCityName: true,
-  showHeaderBreak: false,
-  onKeyDown: () => {},
-  focusReferences: () => {},
-  focusable: false
+  showImage: true
 };
 DestinationSpace.componentName = "DestinationSpace";
 
--- helpers/destinationSpaceSettings.js
+++ helpers/destinationSpaceSettings.js
@@ -16,61 +16,54 @@ export default {
       alignItems: "center",
       justifyContent: "center"
     },
-    header: {
+    itemSettings: {
+      flavor: "yellowInverted",
+      rounded: "3",
+      padding: "3",
+      marginHeight: "1",
       fontWeight: "bold",
-      marginTop: "2"
-    },
-    componentSettings: {
-      itemSettings: {
-        flavor: "yellowInverted",
-        // here
-        rounded: "3",
-        padding: "3",
-        marginHeight: "1",
+      fareBodyItem: {
+        flex: "2",
+        display: "flex",
+        flexDirection: "column"
+      },
+      fareFooterItem: {
+        flex: "1",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "end"
+      },
+      destinationStyles: {
+        fontSize: "5",
+        textTransform: "uppercase",
+        marginBottom: "auto"
+      },
+      departureReturnDateStyles: {
+        separator: "-",
+        separatorStyles: {
+          paddingWidth: "1"
+        }
+      },
+      rocketTypeStyles: {
+        separator: "|",
+        separatorStyles: {
+          paddingWidth: "2"
+        }
+      },
+      callToActionStyles: {
+        callToActionText: "Let's go",
         fontWeight: "bold",
-        fareBodyItem: {
-          flex: "2",
-          display: "flex",
-          flexDirection: "column"
-        },
-        fareFooterItem: {
-          flex: "1",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "end"
-        },
-        destinationStyles: {
-          fontSize: "5",
-          textTransform: "uppercase",
-          marginBottom: "auto"
-        },
-        departureReturnDateStyles: {
-          separator: "-",
-          separatorStyles: {
-            paddingWidth: "1"
-          }
-        },
-        rocketTypeStyles: {
-          separator: "|",
-          separatorStyles: {
-            paddingWidth: "2"
-          }
-        },
-        callToActionStyles: {
-          callToActionText: "Let's go",
-          fontWeight: "bold",
-          textTransform: "uppercase",
-          rounded: "0",
-          paddingWidth: "5"
-        },
-        priceStyles: {
-          width: "100",
-          textAlign: "center",
-          fontSize: "6",
-          marginBottom: "3",
-          currencySymbolStyles: {
-            marginRight: "1"
-          }
+        textTransform: "uppercase",
+        rounded: "0",
+        paddingWidth: "5"
+      },
+      priceStyles: {
+        width: "100",
+        textAlign: "center",
+        fontSize: "6",
+        marginBottom: "3",
+        currencySymbolStyles: {
+          marginRight: "1"
         }
       }
     },
