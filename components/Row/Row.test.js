cef6c882044a9a8004e78d3127f8b84969f1b4b5
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2019-08-27 15:21:40 -0400

    Remove rows and cols

--- components/Row/Row.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'test-utils';
-import Row from './Row';
-
-
-test('render all', () => {
-  const { container } = render(<Row />);
-  expect(container).toMatchSnapshot();
-});
