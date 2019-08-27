29cb7a1d56c6eafd5f233e1657f56dc390386ec2
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2019-08-27 14:23:24 -0400

    Add breakpoints

--- pages/space.js
+++ pages/space.js
@@ -3,6 +3,8 @@ import React from "react";
 import DestinationSpace from "../components/DestinationSpace";
 import Col from "../components/Col";
 import Row from "../components/Row";
+import Text from "../components/Text";
+import Box from "../components/Box";
 
 const Home = () => (
   <div>
@@ -15,7 +17,7 @@ const Home = () => (
     </Head>
 
     <div className="fr-row">
-      <div className="fr-col-4 fr-offset-4">
+      <div className="fr-col-lg-6 fr-offset-lg-3 fr-col-xl-4 fr-offset-xl-4">
         <h1 className="title">Welcome to Destination Space!</h1>
         <DestinationSpace
           image="https://images.unsplash.com/photo-1472712739516-7ad2b786e1f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
@@ -76,8 +78,10 @@ const Home = () => (
               currencyCode: "M"
             }
           ]}
-          translate={() => {}}
         />
+        <Box textAlign="center">
+          <Text>Made with love by Front10</Text>
+        </Box>
       </div>
     </div>
 
