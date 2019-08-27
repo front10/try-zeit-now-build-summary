c2946844f268a4e44e296781d2b388b1b3183d5f
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2019-08-27 15:20:53 -0400

    Layout with flexbox

--- pages/space.js
+++ pages/space.js
@@ -15,83 +15,81 @@ const Home = () => (
         rel="stylesheet"
       />
     </Head>
-    <div className="fr-container-fluid">
-      <div className="fr-row">
-        <div className="fr-col-lg-6 fr-offset-lg-3 fr-col-xl-4 fr-offset-xl-4">
-          <h1 className="title fr-text-secondary">
-            <img
-              className="logo"
-              with="32"
-              height="32"
-              alt=""
-              src="/static/front10.png"
-            />
-            Build summary in the space
-          </h1>
-          <DestinationSpace
-            image="https://images.unsplash.com/photo-1472712739516-7ad2b786e1f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-            name="The Space"
-            imageProps={{
-              labelStyle: {
-                fontSize: "8",
-                textTransform: "uppercase",
-                fontWeight: "bold",
-                textColor: "white"
-              },
-              rounded: "3",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-            header={{
-              fontWeight: "bold",
-              marginTop: "2"
-            }}
-            componentSettings={{
-              itemSettings: {
-                flavor: "basic"
-              }
-            }}
-            data={[
-              {
-                destination: "To Mars",
-                departureDate: "03/13/2020",
-                returnDate: "04/04/2020",
-                rocket: "Falcon 9",
-                weight: 4020,
-                measureUnit: "kg",
-                price: "62",
-                currencySymbol: "$",
-                currencyCode: "M"
-              },
-              {
-                destination: "To Saturn",
-                departureDate: "07/13/2020",
-                returnDate: "09/09/2020",
-                rocket: "Falcon Heavy",
-                weight: 168000,
-                measureUnit: "kg",
-                price: "90",
-                currencySymbol: "$",
-                currencyCode: "M"
-              },
-              {
-                destination: "To Jupiter",
-                departureDate: "11/13/2020",
-                returnDate: "10/03/2020",
-                rocket: "Dragon",
-                weight: 6000,
-                measureUnit: "kg",
-                price: "78",
-                currencySymbol: "$",
-                currencyCode: "M"
-              }
-            ]}
+    <div className="layout">
+      <div className="container">
+        <h1 className="title fr-text-secondary">
+          <img
+            className="logo"
+            with="32"
+            height="32"
+            alt=""
+            src="/static/front10.png"
           />
-          <Box textAlign="center">
-            <Text>Made with 🖤 by Front10</Text>
-          </Box>
-        </div>
+          Build summary in the space
+        </h1>
+        <DestinationSpace
+          image="https://images.unsplash.com/photo-1472712739516-7ad2b786e1f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+          name="The Space"
+          imageProps={{
+            labelStyle: {
+              fontSize: "8",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              textColor: "white"
+            },
+            rounded: "3",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+          header={{
+            fontWeight: "bold",
+            marginTop: "2"
+          }}
+          componentSettings={{
+            itemSettings: {
+              flavor: "basic"
+            }
+          }}
+          data={[
+            {
+              destination: "To Mars",
+              departureDate: "03/13/2020",
+              returnDate: "04/04/2020",
+              rocket: "Falcon 9",
+              weight: 4020,
+              measureUnit: "kg",
+              price: "62",
+              currencySymbol: "$",
+              currencyCode: "M"
+            },
+            {
+              destination: "To Saturn",
+              departureDate: "07/13/2020",
+              returnDate: "09/09/2020",
+              rocket: "Falcon Heavy",
+              weight: 168000,
+              measureUnit: "kg",
+              price: "90",
+              currencySymbol: "$",
+              currencyCode: "M"
+            },
+            {
+              destination: "To Jupiter",
+              departureDate: "11/13/2020",
+              returnDate: "10/03/2020",
+              rocket: "Dragon",
+              weight: 6000,
+              measureUnit: "kg",
+              price: "78",
+              currencySymbol: "$",
+              currencyCode: "M"
+            }
+          ]}
+        />
+        <Box textAlign="center">
+          <Text>Made with 🖤 by Front10</Text>
+        </Box>
       </div>
     </div>
 
@@ -105,6 +103,13 @@ const Home = () => (
         }
         .title {
           font-weight: normal;
+        }
+        .layout {
+          display: flex;
+          justify-content: center;
+        }
+        .container {
+          width: 600px;
         }
         .logo {
           vertical-align: -5px;
