bee1a611a2c0ae255d8a17c7b3d4743d7ec17439
Ivan <belzee92@gmail.com>
2019-08-27 13:51:15 -0400

    fix layout

--- pages/space.js
+++ pages/space.js
@@ -14,73 +14,71 @@ const Home = () => (
       />
     </Head>
 
-    <div className="hero">
-      <h1 className="title">Welcome to Destination Space!</h1>
-      <Row>
-        <Col md="6">
-          <DestinationSpace
-            image="https://images.unsplash.com/photo-1472712739516-7ad2b786e1f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-            name="The Space"
-            imageProps={{
-              labelStyle: {
-                fontSize: "8",
-                textTransform: "uppercase",
-                fontWeight: "bold",
-                textColor: "white"
-              },
-              rounded: "3",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-            header={{
+    <div className="fr-row">
+      <div className="fr-col-4 fr-offset-4">
+        <h1 className="title">Welcome to Destination Space!</h1>
+        <DestinationSpace
+          image="https://images.unsplash.com/photo-1472712739516-7ad2b786e1f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+          name="The Space"
+          imageProps={{
+            labelStyle: {
+              fontSize: "8",
+              textTransform: "uppercase",
               fontWeight: "bold",
-              marginTop: "2"
-            }}
-            componentSettings={{
-              itemSettings: {
-                flavor: "basic"
-              }
-            }}
-            data={[
-              {
-                destination: "To Mars",
-                departureDate: "03/13/2020",
-                returnDate: "04/04/2020",
-                rocket: "Falcon 9",
-                weight: 4020,
-                measureUnit: "kg",
-                price: "62",
-                currencySymbol: "$",
-                currencyCode: "M"
-              },
-              {
-                destination: "To Saturn",
-                departureDate: "07/13/2020",
-                returnDate: "09/09/2020",
-                rocket: "Falcon Heavy",
-                weight: 168000,
-                measureUnit: "kg",
-                price: "90",
-                currencySymbol: "$",
-                currencyCode: "M"
-              },
-              {
-                destination: "To Jupiter",
-                departureDate: "11/13/2020",
-                returnDate: "10/03/2020",
-                rocket: "Dragon",
-                weight: 4020,
-                measureUnit: "kg",
-                price: "78",
-                currencySymbol: "$",
-                currencyCode: "M"
-              }
-            ]}
-            translate={() => {}}
-          />
-        </Col>
-      </Row>
+              textColor: "white"
+            },
+            rounded: "3",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+          header={{
+            fontWeight: "bold",
+            marginTop: "2"
+          }}
+          componentSettings={{
+            itemSettings: {
+              flavor: "basic"
+            }
+          }}
+          data={[
+            {
+              destination: "To Mars",
+              departureDate: "03/13/2020",
+              returnDate: "04/04/2020",
+              rocket: "Falcon 9",
+              weight: 4020,
+              measureUnit: "kg",
+              price: "62",
+              currencySymbol: "$",
+              currencyCode: "M"
+            },
+            {
+              destination: "To Saturn",
+              departureDate: "07/13/2020",
+              returnDate: "09/09/2020",
+              rocket: "Falcon Heavy",
+              weight: 168000,
+              measureUnit: "kg",
+              price: "90",
+              currencySymbol: "$",
+              currencyCode: "M"
+            },
+            {
+              destination: "To Jupiter",
+              departureDate: "11/13/2020",
+              returnDate: "10/03/2020",
+              rocket: "Dragon",
+              weight: 4020,
+              measureUnit: "kg",
+              price: "78",
+              currencySymbol: "$",
+              currencyCode: "M"
+            }
+          ]}
+          translate={() => {}}
+        />
+      </div>
     </div>
 
     <style jsx global>
